3773f904246ec5571db3291aa15fa160bc2ba909
vikramdsa <76751032+vikramdsa@users.noreply.github.com>
2022-03-10 01:02:19 +0530

    Part 3: Failing test case

--- RestaurantTest.java
+++ RestaurantTest.java
@@ -16,6 +16,8 @@ class RestaurantTest {
         restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToOrder("Vegetable lasagne",319);
+        restaurant.addToOrder("Chicken lasagne",419);
     }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -56,5 +58,28 @@ class RestaurantTest {
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+    //<<<<<<<<<<<<<<<<<<<<<<<OrderValue>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void adding_orderitem_to_orderlist_should_increase_orderlist_size_by_1(){
+        int initialorderlistSize = restaurant.getOrderItemList().size();
+        restaurant.addToOrder("Sizzling brownie",319);
+        assertEquals(initialorderlistSize+1,restaurant.getOrderItemList().size());
+    }
+    @Test
+    public void removing_orderitem_from_orderlist_should_decrease_orderlist_size_by_1() throws orderItemNotFoundException {
+        int initialorderlistSize = restaurant.getOrderItemList().size();
+        restaurant.removeFromOrder("Vegetable lasagne");
+        assertEquals(initialorderlistSize-1,restaurant.getOrderItemList().size());
+    }
+    @Test
+    public void removing_orderitem_that_does_not_exist_should_throw_exception() {
+        assertThrows(orderItemNotFoundException.class,
+                ()->restaurant.removeFromOrder("French fries"));
+    }
 
+    @Test
+    public void get_order_value_from_orderlist() {
+        assertEquals(738,restaurant.orderValue());
+    }
+   //<<<<<<<<<<<<<<<<<<<<<<<OrderValue>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 }
